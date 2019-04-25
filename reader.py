8c0b4a132bfbaf27d6bb2dd7cc872598c6b54eb2
THEBESTSZ <william_felipe_tsubota@msn.com>
2019-04-25 10:36:52 -0400

    correcao reader.py

--- reader.py
+++ reader.py
@@ -1,30 +1,20 @@
 import re as regex
 
-texto = '19 2 3\n 4 5 6\n 7 8 9'  # trocar para arquivo
-
-textoarray = texto.split("\n")
-matriz = []
-
-for texto in textoarray:
-  lista = regex.findall('(\d+)', texto)
-  for i in range(len(lista)):
-    lista[i] = int(lista[i])
-  matriz.append(lista)
-
-print(matriz)
-
 class reader:
-	arq = open('inputpath', 'r')
-	texto = arq.read()
-	
-	caracter = texto[0]
-	i = 0
-	matriz = []
+	def createMatrix():
+		
+		arq = open('inputpath', 'r')
+		texto = arq.read()
+		textoarray = texto.split("\n")
+		matriz = []
+		
+		for texto in textoarray:
+  			lista = regex.findall('(\d+)', texto)
+  			for i in range(len(lista)):
+  				lista[i] = int(lista[i])
+  			matriz.append(lista)
+		print(matriz)
+		return matriz
+	matriz2 = createMatrix()
 
-	while (texto[i] < len(caracter))
-		while (texto[i] != '\n')
-		texto = 'l e ga\n l'
-		i = 0
-		while (i < len(texto)):
-  		print(texto[i])
-  		i = i + 1
+	print (matriz2)
